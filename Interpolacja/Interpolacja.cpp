bde7cf9751be229e58589b99c641640d00226a84
drzew <drzew@LAPTOP-83OF2M2C>
2023-10-18 23:40:43 +0200

    Dodaj pliki projektów.

---
+++ Interpolacja/Interpolacja.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+#include <vector>
+
+std::vector <float> function(std::vector <float>* tab, float f, int s) {
+    int i = 1;
+    for (auto it : *tab) {
+        f = pow(3, it);
+        std::cout << i << ".Punkt: x: " << it << ", y: " << f << std::endl;
+        it++;
+        i++;
+    }
+    return *tab;
+}
+
+int main() {
+    int s;
+    float f = 0;
+    float input;
+
+    std::cout << "Podaj ilosc puntkow: ";
+    std::cin >> s;
+
+    if (s > 0) {
+        std::vector <float> tab;
+        tab = {};
+        std::cout << "Podaj wspolrzedne x punktow: " << std::endl;
+
+        for (int i = 0; i < s; i++) {
+            std::cin >> input;
+            tab.push_back(input);
+        }
+
+        function(&tab, f, s);
+    }
+    else {
+        std::cout << "Ilosc punktow nie moze byc mniejsza od 1.";
+        return 0;
+    }
+
+    return 0;
+}
